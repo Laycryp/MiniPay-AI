574d13a566fff3209d44fde7e1b68ceb7e3da572
laycryp <laycryp@hotmail.com>
2026-07-08 15:44:07 +0300

    chore: add specific talent app verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="talentapp:project_verification" content="4bbe66eae2f1f5a048293f07403c7bf28ad9ceeeb4fa208461d43420dac036671b782554706d553288a1b9c74b0cf81a9425fbe4408f75c71de4c2312a61c2cf" />
+      </head>
       <body className="bg-gray-50 text-gray-900 min-h-screen antialiased">
         <Providers>
           {children}
